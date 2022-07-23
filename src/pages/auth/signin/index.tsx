5c8739749d6dd3f80700d2b7867168c317dbb7a7
Isaac Spiegel <isaacspiegel@gmail.com>
2022-07-23 15:25:27 -0500

    Add auth to signin page

--- src/pages/auth/signin/index.tsx
+++ src/pages/auth/signin/index.tsx
@@ -1,4 +1,8 @@
+import { Container, Title } from '@mantine/core';
 import Head from 'next/head';
+import {
+  AuthenticationForm
+} from '../../../components/AuthenticationForm/AuthenticationForm';
 
 export default function SignInPage() {
   return (
@@ -8,6 +12,18 @@ export default function SignInPage() {
         <meta name="description" content="Sign in to your account" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Container size="xs">
+        <Title
+          align="center"
+          pt={80}
+          pb="xl"
+          sx={(theme) => ({ fontFamily: `Greycliff CF, ${theme.fontFamily}`, fontWeight: 900 })}
+        >
+          Welcome back!
+        </Title>
+        <AuthenticationForm shadow="xl"/>
+      </Container>
+
     </>
   );
 }
