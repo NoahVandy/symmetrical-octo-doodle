26ac958dea7c86cc0c60821e83efd2c658ced491
Isaac Spiegel <isaacspiegel@gmail.com>
2022-07-23 15:09:04 -0500

    Create sign in page

---
+++ src/pages/auth/signin/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function SignInPage() {
+  return (
+    <>
+      <Head>
+        <title>PinDrive Sign In</title>
+        <meta name="description" content="Sign in to your account" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
+}
