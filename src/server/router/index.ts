ee6e4af38a88ff58d5e74a78d93e58eac3f1f3a7
Isaac Spiegel <isaacspiegel@gmail.com>
2022-07-23 22:25:58 -0500

    Process file uploads

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -4,11 +4,13 @@ import superjson from "superjson";
 
 import { exampleRouter } from "./example";
 import { authRouter } from "./auth";
+import { uploadRouter } from './upload';
 
 export const appRouter = createRouter()
   .transformer(superjson)
   .merge("example.", exampleRouter)
-  .merge("auth.", authRouter);
+  .merge("auth.", authRouter)
+  .merge('upload.', uploadRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
